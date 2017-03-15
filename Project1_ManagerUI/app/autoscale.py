9cf383d439361625aabb12473bb0b9ffa0cce2dd
npsfsz <npsfsz@gmail.com>
2017-03-15 18:55:33 -0400

    asd

--- Project1_ManagerUI/app/autoscale.py
+++ Project1_ManagerUI/app/autoscale.py
@@ -5,7 +5,24 @@ from operator import itemgetter
 import mysql.connector
 
 from app.config import *
+def connect_to_database():
+    return mysql.connector.connect(user=db_config['user'],
+                                   password=db_config['password'],
+                                   host=db_config['host'],
+                                   database=db_config['database'])
 
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = connect_to_database()
+    return db
+
+
+@webapp.teardown_appcontext
+def teardown_db(exception):
+    db = getattr(g, '_database', None)
+    if db is not None:
+        db.close()
 def background_monitor():
     ec2 = boto3.resource('ec2')
 
