e73a3b4c6ae32a15ee4fe2997ee65ea0847c6102
npsfsz <npsfsz@gmail.com>
2017-03-15 15:42:41 -0400

    asdasdasd

--- Project1_ManagerUI/run.py
+++ Project1_ManagerUI/run.py
@@ -1,6 +1,7 @@
 #!venv/bin/python
 from app import webapp
 from app import autoscale
+import threading
 #webapp.run(host='0.0.0.0',debug=True)
 monitor_process = threading.Thread(target=autoscale.background_monitor)
 monitor_process.start()
